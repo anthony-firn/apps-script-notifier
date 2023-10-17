4854ad40cee2852265905eea0cb91a1b72955b08
Anthony Firn <anthonyfirn@illi.ac>
2023-10-16 20:26:53 -0700

    Shell script

---
+++ native_messaging/native_messaging_host.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/anthony/miniconda3/bin/python3 /home/anthony/Dev/apps-script-notifier/native_messaging/send_gnome_notification.py
--- native_messaging/send_gnome_notification.py
+++ native_messaging/send_gnome_notification.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
-
 import sys
 import json
 import struct
 import subprocess
+import os
+
+LOG_PATH = "/home/anthony/Dev/apps-script-notifier/native_messaging/log.txt"
 
 def send_message(message):
     """Send a message to the web extension."""
@@ -26,10 +27,20 @@ def send_notification(message_text):
     """Send a notification to Gnome."""
     subprocess.run(['notify-send', message_text])
 
+def log(message):
+    """Write log message to the log file."""
+    with open(LOG_PATH, 'a') as log_file:
+        log_file.write(f"{message}\n")
+
 if __name__ == '__main__':
+    log("Script started.")
     message = read_message()
+    log(f"Received message: {message}")
+
     if 'text' in message:
         send_notification(message['text'])
         send_message({'status': 'Notification sent successfully!'})
+        log("Notification sent.")
     else:
         send_message({'error': 'No text found in the received message.'})
+        log("Error: No text found in the received message.")
